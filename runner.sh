5b475b9b0077155fade23b72f135a14876d07436
Yaron Rosenbaum <yaron.rosenbaum@gmail.com>
2015-03-29 17:10:12 +0300

    bad merge..

--- runner.sh
+++ runner.sh
@@ -14,15 +14,10 @@ fi
 
 ln -s /var/log/vpnserver/*_log /usr/local/vpnserver/
 
-<<<<<<< HEAD
 echo "Taking a nap, to allow network to properly set up.."
 sleep 10
-
 echo "That was a good nap. Now to work..."
 
-
-=======
->>>>>>> cb86f0739d134ac446c583a821167e7fa4b19ce4
 exec /usr/local/vpnserver/vpnserver execsvc
 
 exit $?
